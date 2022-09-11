9140476238e7f16eaebeba58f3b6db050f4b2114
AhmetSalihKaragoz <AhmetKaragozz3455@gmail.com>
2022-09-11 21:45:09 +0300

    v1.1

--- Assets/Scripts/FruitSpawner.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FruitSpawner : MonoBehaviour
-{
-    public List<Fruit.FruitType> fruitList = new List<Fruit.FruitType>();
-}
